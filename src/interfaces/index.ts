c10e907052e256304da668fab19041d40365e8e7
webgently <gently039@gmail.com>
2023-10-19 03:43:19 +0300

    feat: declare register and login interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,1 +1,11 @@
-export {}
+export interface RegisterObject {
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+}
+
+export interface LoginObject {
+  email: string;
+  password: string;
+}
