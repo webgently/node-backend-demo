31a53dafb6acf8648df7d3aca7a223542eb63194
webgently <gently039@gmail.com>
2023-10-19 04:26:23 +0300

    update: serve structure

--- src/serve.ts
+++ src/serve.ts
@@ -1,24 +1,39 @@
-import express, {Express} from "express";
-import cors from "cors";
+require('dotenv').config();
+import http from 'http';
+import express, { Express } from 'express';
+import cors from 'cors';
 
+// External Modules
+import API from './apis';
 import config from './config';
 import ConnectDatabase from './config/database';
 
+// Get router
+const router = express.Router();
+
 const app: Express = express();
-const port: Number = Number(process.env.HTTP_PORT || 2083);
+const port: Number = Number(process.env.HTTP_PORT || 5000);
+const server = http.createServer(app);
 
-app.use(
-    cors({
-        origin: "*",
-        methods: ["POST", "GET"],
-    })
-);
-
+app.use(cors({ origin: '*', methods: ['POST', 'GET'] }));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+// Frontend Load
+app.use(express.static(__dirname + '/build'));
+app.get('/*', function (req: any, res: any) {
+  res.sendFile(__dirname + '/build/index.html', function (err: any) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+// API Router
+API(router);
+app.use('/api', router);
 
 ConnectDatabase(config.mongoURI);
-
-app.listen(port, () => {
-    console.log(`Server listening on http://localhost:${port}`);
+server.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
 });
