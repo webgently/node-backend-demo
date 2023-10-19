041a245ddf024a5985d8fbd8eab7d14404041344
webgently <gently039@gmail.com>
2023-10-19 04:02:41 +0300

    feat: import auth functions and declare router

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,1 +1,9 @@
-export {}
+import AUTH from './auth';
+
+const API = (router: any) => {
+  // APIs for Auth
+  router.post('/register', AUTH.register);
+  router.post('/login', AUTH.login);
+};
+
+export default API;
